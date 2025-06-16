77d8e4bb41ffe168da520fdda857fa6d2b23bc7f
Ellio13 <ellen.fabel@gmail.com>
2025-06-16 18:43:52 -0400

    added vendor-edit and vendor-list components

--- src/app/feature/vendor/vendor-edit/vendor-edit.ts
+++ src/app/feature/vendor/vendor-edit/vendor-edit.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Vendor } from '../../../model/vendor';
+import { VendorService } from '../../../service/vendor-service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-vendor-edit',
@@ -6,6 +10,45 @@ import { Component } from '@angular/core';
   templateUrl: './vendor-edit.html',
   styleUrl: './vendor-edit.css'
 })
-export class VendorEdit {
+export class VendorEdit implements OnInit, OnDestroy {
+  title: string = "Vendor-Edit";
+  subscription!: Subscription;
+  vendor!: Vendor;
+  vendorId!: number;
 
+
+constructor(
+  private vendorSvc: VendorService,
+  private router: Router,
+  private actRoute: ActivatedRoute
+) {}
+
+ngOnInit(): void {
+    this.actRoute.params.subscribe((parms)=>{
+      this.vendorId = parms['id'];
+      this.subscription = this.vendorSvc.getById(this.vendorId).subscribe({
+      next: (resp) => {
+          this.vendor = resp;
+      },
+      error: (err) => {
+        console.log("Error retrieving vendor for id: " +this.vendorId, err);
+      }
+    });
+  })
+}
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
+    save() {
+    this.vendorSvc.update(this.vendor).subscribe({
+      next: (resp) => {
+        this.vendor = resp;
+        this.router.navigateByUrl('/vendor-list');
+      },
+      error: (err) => {
+        console.log('error saving vendor', err);
+      }
+    });
+  }
 }
